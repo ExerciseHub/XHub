2256d5d228146473f0b3e489a89b404fca907ee1
AMinSC <cdtjdcjf@gmail.com>
2023-08-20 13:46:01 +0900

    기본 urls 세팅

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('player/', include('player.urls')),
+    path('quickmatch/', include('quickmatch.urls')),
+    path('board/', include('board.urls')),
 ]
