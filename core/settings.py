2cf7ed1cb1e0101e14a16be69cc3d49c7489b5e8
AMinSC <cdtjdcjf@gmail.com>
2023-08-20 13:45:19 +0900

    app 추가

--- core/settings.py
+++ core/settings.py
@@ -49,8 +49,11 @@ INSTALLED_APPS = [
     # 프레임워크 및 라이브러리?
     'rest_framework',
     'rest_framework_simplejwt',
+    
     # 내부 기능(앱)
     'player',
+    'quickmatch',
+    'board',
 ]
 
 # REST_Framework
