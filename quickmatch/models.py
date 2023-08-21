1e418a0f0df9e17d3b4e2d174902684bcbd29f7d
YuJinsoo <realaqua23@gmail.com>
2023-08-21 23:04:23 +0900

    quickmatch endpoint 작업

--- quickmatch/models.py
+++ quickmatch/models.py
@@ -9,7 +9,7 @@ class Meeting(models.Model):
     title = models.CharField(max_length=100)
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
-    age = models.DateField(blank=True, null=True)
+    age_limit = models.DateField(blank=True, null=True)
     rating = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -17,10 +17,10 @@ class Meeting(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICE, max_length=50, blank=True, null=True)
     
     GENDER_CHOICE = (("M", "남"), ("W", "여"), ("X", "무관"))
-    gender = models.CharField(choices=GENDER_CHOICE, max_length=50)
+    gender_limit = models.CharField(choices=GENDER_CHOICE, max_length=50)
     
     STATUS_CHOICE = (("모집중", "모집중"), ("모집완료", "모집완료"), ("모집종료", "모집종료"), ("취소", "취소"))
-    status = models.CharField(choices=STATUS_CHOICE, max_length=50)
+    status = models.CharField(choices=STATUS_CHOICE, max_length=50, default="모집중")
 
     location = models.CharField(max_length=255)  # 고민
     # 참여자
