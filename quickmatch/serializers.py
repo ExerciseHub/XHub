8e1ece85da4bdd354b1cb03c586ac25a517d55a2
YuJinsoo <realaqua23@gmail.com>
2023-08-22 14:02:58 +0900

    미팅 생성기능 추가

--- quickmatch/serializers.py
+++ quickmatch/serializers.py
@@ -5,9 +5,8 @@ from .models import Meeting, MeetingChat
 class MeetingSerializer(serializers.ModelSerializer):
     class Meta():
         model = Meeting
-        # TODO - 선택필요
         fields = '__all__'
     
     def create(self, validated_data):
-        meeting = Meeting.objects.create(**validated_data)
+        meeting = Meeting(**validated_data)
         return meeting
--- quickmatch/views.py
+++ quickmatch/views.py
@@ -22,23 +22,33 @@ class CreateMeeting(APIView):
     def post(self, request):
         ## TODO - endpoint 작업
         qdict = request.data
-        print(qdict)
-        data = qdict.items()
+        serializer = MeetingSerializer(data=qdict)
         
-        print(data)
-        serializer = MeetingSerializer(data=data)
-        # print(serializer)
-        print(serializer.is_valid())
         # serializer.organizer = request.user
         if serializer.is_valid():
             
-            print(serializer.data)
-            meeting = serializer.create()
-            print(meeting)
-            # meeting.save()
+            data = serializer.data
+            meeting = serializer.create(data) # 오브젝트 생성만
+            meeting.organizer = request.user
+            
+            category_list = [i[0] for i in Meeting.CATEGORY_CHOICE]
+            gender_list = [i[0] for i in Meeting.GENDER_CHOICE]
+            status_list = [i[0] for i in Meeting.STATUS_CHOICE]
+            
+            # 카테고리, 성별제한, 상태 default설정. (목록에 없는 값은 is_valid Fail)
+            if not data.get('category', None):
+                meeting.category = category_list[0]
+                
+            if not data.get('gender_limit', None):
+                meeting.category = gender_list[2]
+                
+            if not data.get('status', None):
+                meeting.status = status_list[0]
+
+            meeting.save() # 오브젝트 저장
             return Response({"data": "valid"}, status=status.HTTP_200_OK)
         
-        return Response({"message": "data is not available"})
+        return Response({"message": "data is not available", "error": serializer.errors})
 
 
 class DeleteMeeting(APIView):
