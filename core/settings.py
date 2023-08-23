3ccdb232d4183a5003025a63fcd14dfe12db7183
Chesed Kim <starmireu@gmail.com>
2023-08-23 21:28:42 +0900

    nginx.conf & settings.py - staticfiles path

--- core/settings.py
+++ core/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# STATIC FILES
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
