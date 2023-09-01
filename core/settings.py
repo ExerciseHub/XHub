93b55cf6e0a04289a2660b17ed9af81b2e17cf51
Ubuntu <ubuntu@ip-172-26-10-195.ap-northeast-1.compute.internal>
2023-09-01 11:34:21 +0900

    ALLOWED_HOSTS update

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['daphne', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['daphne', 'localhost', '127.0.0.1', '54.248.217.183']
 
 
 AUTHENTICATION_BACKENDS = (
