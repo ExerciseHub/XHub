b7366f0e3024343d3cc34f541be2e64a199c2c6e
YuJinsoo <realaqua23@gmail.com>
2023-09-13 23:29:26 +0900

    채팅메시지 종류 구분 가능하도록 개발, 입장/퇴장 메시지 추가, db중복반영 수정

--- quickmatch/consumers.py
+++ quickmatch/consumers.py
@@ -1,9 +1,13 @@
 import json
 
+from django.contrib.auth import get_user_model
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 
 from .models import MeetingMembers, MeetingMessage, MeetingRoom
+
+
+User = get_user_model()
 
 
 class MeetingRoomConsumer(AsyncWebsocketConsumer):
@@ -20,16 +24,22 @@ class MeetingRoomConsumer(AsyncWebsocketConsumer):
         )
         await self.accept()
         
-        await self.channel_layer.group_send({
-            'msg_type' : 'info',
-            'message' : f'{self.user.email} 님이 채팅에 참석했습니다.'
-        })
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'room_info_message',
+                'message' : f'{self.user} joined the chat.'
+            }
+        )
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_send({
-            'msg_type' : 'info',
-            'message' : f'{self.user.email} 님이 채팅에tj 떠났습니다.'
-        })
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'room_info_message',
+                'message' : f'{self.user} has left the chat.'
+            }
+        )
         
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -40,6 +50,8 @@ class MeetingRoomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        sender_email = text_data_json['sender_email']
+        # sender_id = text_data_json['sender_id']
         
         self.members_email = await self.get_members_email()
         
@@ -50,9 +62,9 @@ class MeetingRoomConsumer(AsyncWebsocketConsumer):
                 {
                     'type': 'room_chat_message',
                     'message': message,
+                    'sender_email': sender_email,
                     'room_id' : self.room_name,
                     'group_member': self.members_email,
-                    'sender_email': self.user.email
                 }
             )
         else:
@@ -61,20 +73,31 @@ class MeetingRoomConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def room_chat_message(self, event):
         message = event['message']
+        sender_email = event['sender_email']
         room_id = event['room_id']
         group_member = event['group_member']
-        user_email = event['sender_email']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'msg_type' : 'message',
             'message': message,
+            'sender_email': sender_email,
             'room_id': room_id,
-            'group_member': group_member,
-            'sender_email': user_email
+            'group_member': group_member
         }))
+        
         # save to database
-        await self.save_message(user=self.user, room_id=room_id, message=message)
+        if self.user.email == sender_email:
+            await self.save_message(user=self.user.id, room_id=room_id, message=message)
+    
+    async def room_info_message(self, event):
+        message = event['message']
+        
+        await self.send(text_data=json.dumps({
+            'msg_type' : 'info',
+            'message': message,
+            'sender_email': 'system'
+        }))
     
     @database_sync_to_async
     def get_members_email(self):
@@ -84,7 +107,7 @@ class MeetingRoomConsumer(AsyncWebsocketConsumer):
     
     @database_sync_to_async
     def save_message(self, user, room_id, message):
-        user = User.objects.get(pk=user)
+        user = User.objects.get(id=user)
         meetingroom = MeetingRoom.objects.get(meeting=room_id)
         
         MeetingMessage.objects.create(room=meetingroom, user=user, content=message)
